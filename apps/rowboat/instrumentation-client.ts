c79a3dde4b815b45f20b701b21b1aeb2a6d17a66
Ramnique Singh <30795890+ramnique@users.noreply.github.com>
2025-09-15 17:50:10 +0530

    wip

--- apps/rowboat/instrumentation-client.ts
+++ apps/rowboat/instrumentation-client.ts
@@ -1,7 +1,10 @@
 // instrumentation-client.js
 import posthog from 'posthog-js';
+import { captureRouterTransitionStart} from "@sentry/nextjs";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: '2025-05-24'
 });
+
+export const onRouterTransitionStart = captureRouterTransitionStart;
