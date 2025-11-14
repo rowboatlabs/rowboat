61924d0b01c0700f9c3c0b12092b3bc9ea133198
Ramnique Singh <30795890+ramnique@users.noreply.github.com>
2025-11-14 09:13:58 +0530

    remove test.ts

--- apps/cli/src/test.ts
+++
@@ -1,5 +0,0 @@
-import { RunEvent } from "./application/entities/workflow-event.js";
-
-const obj = {"type":"tool-invocation","stepId":"test_agent","toolName":"ask-human","input":{"question":"Do you want me to run the command `date` in the terminal to show today’s date?"},"ts":"2025-11-11T06:31:20.103Z"};
-
-console.log(RunEvent.parse(obj));
