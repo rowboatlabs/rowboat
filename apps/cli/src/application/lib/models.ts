6357126b8a03609391c39e169e9a2653cf0945ec
Ramnique Singh <30795890+ramnique@users.noreply.github.com>
2025-11-19 01:03:43 +0530

    wip

--- apps/cli/src/application/lib/models.ts
+++ apps/cli/src/application/lib/models.ts
@@ -1,9 +1,10 @@
 import { createOpenAI, OpenAIProvider } from "@ai-sdk/openai";
 import { createGoogleGenerativeAI, GoogleGenerativeAIProvider } from "@ai-sdk/google";
 import { AnthropicProvider, createAnthropic } from "@ai-sdk/anthropic";
+import { OllamaProvider, createOllama } from "ollama-ai-provider-v2";
 import { ModelConfig } from "../config/config.js";
 
-const providerMap: Record<string, OpenAIProvider | GoogleGenerativeAIProvider | AnthropicProvider> = {};
+const providerMap: Record<string, OpenAIProvider | GoogleGenerativeAIProvider | AnthropicProvider | OllamaProvider> = {};
 
 export function getProvider(name: string = "") {
     if (!name) {
@@ -21,18 +22,27 @@ export function getProvider(name: string = "") {
             providerMap[name] = createOpenAI({
                 apiKey: providerConfig.apiKey,
                 baseURL: providerConfig.baseURL,
+                headers: providerConfig.headers,
             });
             break;
         case "anthropic":
             providerMap[name] = createAnthropic({
                 apiKey: providerConfig.apiKey,
                 baseURL: providerConfig.baseURL,
+                headers: providerConfig.headers,
             });
             break;
         case "google":
             providerMap[name] = createGoogleGenerativeAI({
                 apiKey: providerConfig.apiKey,
                 baseURL: providerConfig.baseURL,
+                headers: providerConfig.headers,
+            });
+            break;
+        case "ollama":
+            providerMap[name] = createOllama({
+                baseURL: providerConfig.baseURL,
+                headers: providerConfig.headers,
             });
             break;
     }
