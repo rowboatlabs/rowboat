33906f3a1606ac352fa84fed165c0a4c2031ec85
tusharmagar <tushmag@gmail.com>
2026-09-06 23:16:08 +0530

    perf(renderer): migrate class merging to cn

--- apps/x/apps/renderer/src/lib/utils.ts
+++ apps/x/apps/renderer/src/lib/utils.ts
@@ -1,9 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export { cn } from "cn";
 
 /**
  * Compact an absolute path to a right-anchored tail of whole segments within
